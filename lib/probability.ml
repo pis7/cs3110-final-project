68869d7bf0d289e46e8ad631592eeb2775a5a662
Edward Liu <ejl248@cornell.edu>
2024-03-13 13:00:06 -0400

    Add interface for probability, add probability func
    - created interface file for probability
    - fixed permutation, added combination
    - made functions go from float arguments to floats

--- lib/probability.ml
+++ lib/probability.ml
@@ -1,16 +1,17 @@
 let () = Random.self_init ()
-(** [rand n] is [n] random numbers in [0,1]*)
-let rec rand_tr n acc = match n with 
+
+let rec rand_tr n acc =
+  match n with
   | 0 -> acc
-  | _ -> rand_tr (n-1) (Random.float 1. :: acc)
-let rand n = rand_tr n []
+  | _ -> rand_tr (n - 1) (Random.float 1. :: acc)
 
-let rec fact_tr n acc = fact_tr (n-1) (n * acc)
+let rand n = rand_tr (int_of_float n) []
 
-(** [factorial n] is [n] factorial*)
-let factorial n = fact_tr n 0
+let rec fact_tr n acc =
+  match n with
+  | 0 -> acc
+  | _ -> fact_tr (n - 1) (n * acc)
 
-(** [permutation n r] is [n]P[r] *)
-let permutation n r = factorial n / (factorial (n-r) * factorial(r))
-
-(** *)
+let factorial n = float_of_int (fact_tr (int_of_float n) 1)
+let permutation n r = factorial n /. factorial (n -. r)
+let combination n r = factorial n /. (factorial (n -. r) *. factorial r)
---
+++ lib/probability.mli
@@ -0,0 +1,11 @@
+val rand : float -> float list
+(** [rand n] is [n] random numbers in [0,1]*)
+
+val factorial : float -> float
+(** [factorial n] is [n] factorial*)
+
+val permutation : float -> float -> float
+(** [permutation n r] is [n]P[r] *)
+
+val combination : float -> float -> float
+(** [combination n r] is [n]C[r]*)
