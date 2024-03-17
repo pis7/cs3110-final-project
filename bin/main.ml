d5cbdd44c703530f110af0fc71d1d67b617149fd
George <gpm58@cornell.edu>
2024-03-17 19:25:12 -0400

    Started Stuff on Main

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,34 @@
-let () = print_endline "Hello, World!"
+open Final_project.Help
+
+let display_menu () =
+  print_endline "Welcome to the ArduinBro's Calculator!";
+  print_endline "";
+  print_endline "";
+  print_endline "Below are the current Modules offered:";
+  print_endline "| Plot: Plot a function you want! |";
+  print_endline
+    "| Eval: Basic calculations and operations offered by the calculator |";
+  print_endline
+    "| Settings: Change certain features of the calculator when solving \
+     problems |";
+  print_endline
+    "| Help: If you are not sure how to use any features, do --help |";
+  print_endline "\nBegin:"
+
+let rec process_input () =
+  print_endline "Enter a command:";
+  match read_line () with
+  | "quit" -> print_endline "Exiting calculator."
+  | "--help" ->
+      print_endline
+        "Enter the section you need help with (plot, eval, settings):";
+      let section = read_line () in
+      help_menu section;
+      process_input ()
+  | _ -> process_input ()
+
+let main () =
+  display_menu ();
+  process_input ()
+
+let () = main ()
---
+++ lib/help.ml
@@ -0,0 +1,6 @@
+let help_menu (section : string) =
+  match section with
+  | "plot" -> print_endline "more to come"
+  | "eval" -> print_endline "more to come"
+  | "settings" -> print_endline "more to come"
+  | _ -> print_endline "Enter a valid option: <plot>, <eval>, <settings>"
