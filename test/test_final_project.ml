595be1b8c6ae4a13757b3c3da5ab1768e02b3fae
Edward Liu <ejl248@cornell.edu>
2024-03-20 01:19:39 -0400

    Covered Probability with unit tests
    - achieved 100% coverage in probability unit tests
    - got bisect working

--- test/test_final_project.ml
+++ test/test_final_project.ml
@@ -6,6 +6,22 @@ let probability_tests =
       assert_equal 1. (Final_project.Probability.factorial 1.) );
     ( "factorial 4." >:: fun _ ->
       assert_equal 24. (Final_project.Probability.factorial 4.) );
+    ( "rand 1." >:: fun _ ->
+      assert_equal true
+        (let num = Final_project.Probability.rand 1. in
+         match num with
+         | [] -> false
+         | h :: _ -> 0. <= h && h <= 1.) );
+    ( "rand 2." >:: fun _ ->
+      assert_equal true
+        (let num = Final_project.Probability.rand 2. in
+         match num with
+         | h :: h2 :: _ -> 0. <= h && h <= 1. && 0. <= h2 && h2 <= 1.
+         | _ -> false) );
+    ( "permutation 4. 2." >:: fun _ ->
+      assert_equal 12. (Final_project.Probability.permutation 4. 2.) );
+    ( "combination 4. 2." >:: fun _ ->
+      assert_equal 6. (Final_project.Probability.combination 4. 2.) );
   ]
 
 let tests = [ ("a trivial test" >:: fun _ -> assert_equal 0 0) ]
