d3359871e6b52b49a8fe98718dee7d51bde28ff9
Edward Liu <ejl248@cornell.edu>
2024-03-20 01:58:12 -0400

    Added additional arithmetic functions
    - float version of add, sub, div, mult
    - other buttons functions on left side added
    - pi and e constants added

--- lib/arithmetic.ml
+++ lib/arithmetic.ml
@@ -1,22 +1,17 @@
-(** [add_i a b] is [a] added to [b] *)
 let add_i a b = a + b
-
-let%test "add_i" =
-  3 = add_i 1 2
-
-(** [sub_i a b] is [a] - [b] *)
 let sub_i a b = a - b
-
-let%test "sub_i" =
-  ~-1 = sub_i 1 2
-
-(** [mult_i a b] is [a] * [b]*)
 let mult_i a b = a * b
-
-let%test "mult_i" =
-  6 = mult_i 3 2
-
-(** [div_i a b] is integer division [a] / [b]*)
 let div_i a b = a / b
-
-let%test "div_i" = 2 = div_i 6 3
+let add a b = a +. b
+let sub a b = a -. b
+let mult a b = a *. b
+let div a b = a /. b
+let inverse a = 1. /. a
+let square a = a *. a
+let sqrt a = a ** 0.5
+let log a = Float.log10 a
+let ten_x a = 10. ** a
+let ln a = Float.log a
+let exp a = Float.exp a
+let pi = Float.pi
+let e = Float.exp 1.
---
+++ lib/arithmetic.mli
@@ -0,0 +1,50 @@
+val add_i : int -> int -> int
+(** [add_i a b] is [a] added to [b] *)
+
+val sub_i : int -> int -> int
+(** [sub_i a b] is [a] - [b] *)
+
+val mult_i : int -> int -> int
+(** [mult_i a b] is [a] * [b]*)
+
+val div_i : int -> int -> int
+(** [div_i a b] is integer division [a] / [b]*)
+
+val add : float -> float -> float
+(** [add a b] is [a] added to [b] *)
+
+val sub : float -> float -> float
+(** [sub a b] is [a] - [b] *)
+
+val mult : float -> float -> float
+(** [mult a b] is [a] * [b]*)
+
+val div : float -> float -> float
+(** [div a b] is float division [a] / [b]*)
+
+val inverse : float -> float
+(** [inverse a] is 1 / [a]*)
+
+val square : float -> float
+(** [square a] is [a]^2*)
+
+val sqrt : float -> float
+(** [sqrt a] is [a]^0.5*)
+
+val log : float -> float
+(** [log a] is log base 10 [a]*)
+
+val ten_x : float -> float
+(** [ten_x a] is 10^[a]*)
+
+val ln : float -> float
+(** [ln a] is natural log of [a]*)
+
+val exp : float -> float
+(** [exp a] is e^[a]*)
+
+val pi : float
+(** [pi] is 3.1415... or Pi *)
+
+val e : float
+(** [e] is 2.718... or e*)
