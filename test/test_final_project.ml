e356f771cbf8b77c237980fb653a697ff5ef98ea
Edward Liu <ejl248@cornell.edu>
2024-03-20 13:47:25 -0400

    Added arithmetic test cases
    - added arithmetic test cases

--- test/test_final_project.ml
+++ test/test_final_project.ml
@@ -24,6 +24,77 @@ let probability_tests =
       assert_equal 6. (Final_project.Probability.combination 4. 2.) );
   ]
 
+let close_enough a b = Float.abs (a -. b) < 0.00000001
+
+let arithmetic_tests =
+  [
+    ( "add_i " >:: fun _ ->
+      assert_equal 2 (Final_project.Arithmetic.add_i 1 1) ~printer:string_of_int
+    );
+    ( "sub_i " >:: fun _ ->
+      assert_equal 1 (Final_project.Arithmetic.sub_i 2 1) ~printer:string_of_int
+    );
+    ( "mult_i " >:: fun _ ->
+      assert_equal 2
+        (Final_project.Arithmetic.mult_i 2 1)
+        ~printer:string_of_int );
+    ( "div_i " >:: fun _ ->
+      assert_equal 2 (Final_project.Arithmetic.div_i 4 2) ~printer:string_of_int
+    );
+    ( "add " >:: fun _ ->
+      assert_equal 2.
+        (Final_project.Arithmetic.add 1. 1.)
+        ~printer:string_of_float );
+    ( "sub " >:: fun _ ->
+      assert_equal 1.
+        (Final_project.Arithmetic.sub 2. 1.)
+        ~printer:string_of_float );
+    ( "mult " >:: fun _ ->
+      assert_equal 2.
+        (Final_project.Arithmetic.mult 2. 1.)
+        ~printer:string_of_float );
+    ( "div " >:: fun _ ->
+      assert_equal 2.
+        (Final_project.Arithmetic.div 4. 2.)
+        ~printer:string_of_float );
+    ( "inverse " >:: fun _ ->
+      assert_equal 2.
+        (Final_project.Arithmetic.inverse 0.5)
+        ~printer:string_of_float );
+    ( "square " >:: fun _ ->
+      assert_equal 4.
+        (Final_project.Arithmetic.square 2.)
+        ~printer:string_of_float );
+    ( "sqrt " >:: fun _ ->
+      assert_equal 2.
+        (Final_project.Arithmetic.sqrt 4.)
+        ~printer:string_of_float );
+    ( "log " >:: fun _ ->
+      assert_equal 2.
+        (Final_project.Arithmetic.log 100.)
+        ~printer:string_of_float );
+    ( "ten_x " >:: fun _ ->
+      assert_equal 100.
+        (Final_project.Arithmetic.ten_x 2.)
+        ~printer:string_of_float );
+    ( "ln " >:: fun _ ->
+      assert_equal true
+        (close_enough (Final_project.Arithmetic.ln 2.) 0.6931471806) );
+    ( "exp " >:: fun _ ->
+      assert_equal true
+        (close_enough (Final_project.Arithmetic.exp 2.) 7.3890560989) );
+    ( "pi " >:: fun _ ->
+      assert_equal Float.pi Final_project.Arithmetic.pi ~printer:string_of_float
+    );
+    ( "exp " >:: fun _ ->
+      assert_equal true (close_enough Final_project.Arithmetic.e 2.7182818285)
+    );
+  ]
+
 let tests = [ ("a trivial test" >:: fun _ -> assert_equal 0 0) ]
-let suite = "sort test suite" >::: List.flatten [ probability_tests; tests ]
+
+let suite =
+  "sort test suite"
+  >::: List.flatten [ probability_tests; arithmetic_tests; tests ]
+
 let _ = run_test_tt_main suite
